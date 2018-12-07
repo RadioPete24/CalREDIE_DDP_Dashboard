a5c9cde7effdefc54d97e8544b353a87a6d1ead7
CDPHINTRA\PIp <Peter.Ip@cdph.ca.gov>
2018-12-07 14:03:58 -0800

    also new table update (still drafted)

---
+++ crFunctions/bivarSummaryTbl.R
@@ -0,0 +1,45 @@
+getBivarSummaryTbl <- function(tmp_df = tmp_df, dtRange = input$dateRange, disShort = input$disease, Rstatus = input$rstat, sex = input$Sex, race = input$Race, ethnicity = input$Ethnicity, ageGrp = input$ageGrp, marital = input$marital, pregnant = input$pregnant
+){
+  tbl_list <- list("Sex", "ageGrp", "Race", "Ethnicity", "RStatus", "Marital", "Pregnant")
+  tmp_df$DtEpisode <- as.Date(tmp_df$DtEpisode, format = "%m/%d/%Y")
+  tmp_df <- tmp_df[(tmp_df$DtEpisode>=dtRange[1] & tmp_df$DtEpisode<=dtRange[2])
+                   & tmp_df$DisShort %in% c(disShort)
+                   & tmp_df$RStatus %in% c(Rstatus)
+                   & tmp_df$Sex %in% c(sex)
+                   & tmp_df$Race %in% c(race)
+                   & tmp_df$Ethnicity %in% c(ethnicity)
+                   & tmp_df$ageGrp %in% c(ageGrp)
+                   & tmp_df$Marital %in% c(marital)
+                   & tmp_df$Pregnant %in% c(pregnant)
+                   ,]
+  summary_tbl <- lst()
+  
+ 
+  for(i in 1:length(tbl_list)){
+               #Data columns
+               summary_tbl[[i]] <- data.frame(Var1 = row.names(cbind(as.data.frame.matrix(
+                 xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)], data = tmp_df))
+                 , as.data.frame.matrix(prop.table(xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)], data = tmp_df)))))
+                 , cbind(as.data.frame.matrix(xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)], data = tmp_df))
+                         , as.data.frame.matrix(round(prop.table(xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)]
+                                                                       , data = tmp_df))*100, 2))))
+
+               # summary_tbl[[i]]$Var1 <- paste("<p style=\"text-indent: 20px\">", as.data.frame(table(tmp_df[as.character(tbl_list[[i]])]))[,1], "</p>")
+               summary_tbl[[i]]$Var1 <- paste("<p style=\"text-indent: 20px\">", summary_tbl[[i]][,1], "</p>")
+
+               temp_df <- cbind(paste0("<b>", as.character(tbl_list[[i]]), "</b>", sep = "")
+                                , data.frame(matrix(NA, ncol = length(data.frame(Var1 = row.names(cbind(as.data.frame.matrix(
+                                  xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)], data = tmp_df))
+                                  , as.data.frame.matrix(prop.table(xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)]
+                                                                          , data = tmp_df)))))
+                                  , cbind(as.data.frame.matrix(xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)]
+                                                                     , data = tmp_df))
+                                          , as.data.frame.matrix(prop.table(xtabs(~tmp_df[,as.character(tbl_list[[i]])] + tmp_df[,as.character(bivariate)]
+                                                                                  , data = tmp_df))))))-1, nrow = 1)))
+
+               colnames(temp_df) <- colnames(summary_tbl[[i]])
+               summary_tbl[[i]] <- rbind(temp_df, summary_tbl[[i]])
+             }
+      summary_tbl <- do.call(rbind, summary_tbl)
+      as.data.frame(summary_tbl)
+}
