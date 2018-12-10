60f8ac3b06f4e685d03cab4489dcfb58c61b1615
CDPHINTRA\PIp <Peter.Ip@cdph.ca.gov>
2018-12-10 11:21:46 -0800

    adding DDG beginning

---
+++ crFunctions/deidentifyGuide.R
@@ -0,0 +1,13 @@
+#Data De-Identification Guide algorithm - for scoring Publication ability of Personal Health Information Data
+getDDG <- function (df){
+  scoreCriteria_df <- data.frame("events"= ,
+                              "sex" = ,
+                              "ageRange" = ,
+                              "race" = ,
+                              "ethnicity" = ,
+                              "language" = ,
+                              "period" = ,
+                              "populationResid" = ,
+                              "populationServ" = ,
+                              "interactions" = )
+}
